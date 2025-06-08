b6cbecdff21323d7975f1afaf73b4abd1f729323
El-Ayvar <=>
2025-06-08 16:30:40 -0600

    terminando proyecto de gifs

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const btnSearch = document.querySelector("#search-btn");
+
+const searchGif = () => {
+    const gifName = document.querySelector("#gif-input").value.trim();
+    if (!gifName) {
+        document.querySelector("#gif-info").innerHTML = `<p>Por favor, ingresa el nombre de un GIF.</p>`;
+        return;
+    }
+
+    const apiKey = "3mK7em9Aeq76yOYBf9IfDqGl8voyN45c";
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${gifName}&limit=20`;
+
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Error al buscar los GIFs");
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data.data.length === 0) {
+                throw new Error("No se encontraron resultados");
+            }
+            showGifs(data.data);
+        })
+        .catch(error => {
+            document.querySelector("#gif-info").innerHTML = `<p id="mensaje-error">${error.message}</p>`;
+        });
+};
+
+function showGifs(gifs) {
+    const gifInfo = document.querySelector("#gif-info");
+    gifInfo.innerHTML = "";
+
+    gifs.forEach(gif => {
+        gifInfo.innerHTML += `
+            <div id="box-gif">
+                <h2>${gif.title.toUpperCase()}</h2>
+                <img src="${gif.images.original.url}" alt="${gif.title}"/>
+            </div>
+        `;
+    });
+}
+btnSearch.addEventListener("click", searchGif);
